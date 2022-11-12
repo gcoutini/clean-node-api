59c6e50fde02860061b10b54e7c111a4a92d993e
gcoutini <61057977+gcoutini@users.noreply.github.com>
2022-11-11 21:52:35 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  coverageProvider: 'v8'
+}
